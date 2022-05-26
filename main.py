97f54f658ca1ea751975f147d30d5d447677b031
PiotrCzerwinski <piotrek26061997@gmail.com>
2022-05-26 20:23:23 +0200

    Dodana została funkcja umożliwiająca wielokrotne wywoływanie obydwu algorytmów za jednym razem

--- main.py
+++ main.py
@@ -7,11 +7,31 @@ import numpy as np
 def fitness_function(solution):
     return np.sum(solution ** 2)
 
+
 def multiplePasses(dict,
                    epoch,
                    pop_size,
+                   repetitions
                    ):
-    return None
+    bfo_solutions = [{} for _ in range(repetitions)]
+    abc_solutions = [{} for _ in range(repetitions)]
+    bfo = OriginalBFO(dict, epoch, pop_size)
+    abc = BaseABC(dict, epoch, pop_size)
+    for i in range(repetitions):
+        best_position_bfo, best_fitness_bfo = bfo.solve()
+        time_bfo = sum(bfo.history.list_epoch_time)
+        bfo_solutions[i]['bp'] = best_position_bfo
+        bfo_solutions[i]['bf'] = best_fitness_bfo
+        bfo_solutions[i]['t'] = time_bfo
+
+        best_position_abc, best_fitness_abc = abc.solve()
+        time_abc = sum(abc.history.list_epoch_time)
+        abc_solutions[i]['bp'] = best_position_abc
+        abc_solutions[i]['bf'] = best_fitness_abc
+        abc_solutions[i]['t'] = time_abc
+
+    print(f"BFO {bfo_solutions}")
+    print(f"ABC {abc_solutions}")
 
 
 if __name__ == '__main__':
@@ -24,32 +44,5 @@ if __name__ == '__main__':
     }
     epoch = 100
     pop_size = 50
-    # ci = 0.01
-    # ped = 0.25
-    # nc = 5
-    # ns = 5
-    # attract_repels = [0.1, 0.2, 0.1, 10]
+    multiplePasses(problem_dict1, epoch, pop_size, 3)
 
-    model = OriginalBFO(problem_dict1,
-                        epoch,
-                        pop_size)
-    best_position_bfo, best_fitness_bfo = model.solve()
-    model.history.save_trajectory_chart("bfo_trajectory")
-
-    print(f"Solution: {best_position_bfo}, Fitness: {best_fitness_bfo}")
-
-    print(sum(model.history.list_epoch_time))
-
-    # bees
-
-    couple_bees = [16, 4]
-    patch_variables = [5, 0.98]
-    sites = [3, 1]
-    abc_model = BaseABC(problem_dict1, epoch, pop_size)
-
-    best_position_abc, best_fitness_abc = abc_model.solve()
-    abc_model.history.save_trajectory_chart("abc_trajectory")
-    print(f"Solution: {best_position_abc}, Fitness: {best_fitness_abc}")
-    print(sum(abc_model.history.list_epoch_time))
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
