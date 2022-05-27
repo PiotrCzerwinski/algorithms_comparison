3bd6cedef6372b074aaa2061198aacfdd8ff1579
PiotrCzerwinski <piotrek26061997@gmail.com>
2022-05-27 13:49:59 +0200

    Dodano funkcję do wielokrotnego wywoływania z podaną funkcją i zapisem do csv

--- main.py
+++ main.py
@@ -4,39 +4,45 @@ from mealpy.swarm_based.ABC import BaseABC
 import numpy as np
 from opfunu.cec_basic.cec2014 import *
 from opfunu.type_based.uni_modal import Functions
+import csv
 
 
-def multiplePasses(dict,
+def multiplePasses(function,
+                   lb,
+                   up,
                    epoch,
                    pop_size,
                    repetitions
                    ):
-    bfo_solutions = [{} for _ in range(repetitions)]
-    abc_solutions = [{} for _ in range(repetitions)]
+    dict = {
+        "fit_func": function,
+        "lb": lb,
+        "ub": up,
+        "minmax": "min",
+        "n_dims": 2
+
+    }
     bfo = OriginalBFO(dict, epoch, pop_size)
     abc = BaseABC(dict, epoch, pop_size)
+    f = open('bfo_abc.csv', 'w')
+    writer = csv.writer(f)
     for i in range(repetitions):
         best_position_bfo, best_fitness_bfo = bfo.solve()
         time_bfo = sum(bfo.history.list_epoch_time)
-        bfo_solutions[i]['bp'] = best_position_bfo
-        bfo_solutions[i]['bf'] = best_fitness_bfo
-        bfo_solutions[i]['t'] = time_bfo
-
+        bfo_row = ["bfo", best_position_bfo[0],best_position_bfo[1], best_fitness_bfo, time_bfo]
         best_position_abc, best_fitness_abc = abc.solve()
         time_abc = sum(abc.history.list_epoch_time)
-        abc_solutions[i]['bp'] = best_position_abc
-        abc_solutions[i]['bf'] = best_fitness_abc
-        abc_solutions[i]['t'] = time_abc
-
-    print(f"BFO {bfo_solutions}")
-    print(f"ABC {abc_solutions}")
+        abc_row = ["abc", best_position_abc[0],best_position_abc[1], best_fitness_abc, time_abc]
+        writer.writerow(bfo_row)
+        writer.writerow(abc_row)
+    f.close()
 
 
 if __name__ == '__main__':
     functions = Functions()
     ## Setting parameters
-    epoch = 50
-    pop_size = 150
+    epoch = 25
+    pop_size = 100
     objective_fun = functions._rosenbrock__
     problem_dict = {
         "fit_func": objective_fun,
@@ -47,19 +53,18 @@ if __name__ == '__main__':
 
     }
 
-    # multiplePasses(problem_dict, epoch, pop_size, 1)
+    multiplePasses(objective_fun, -30, 30, 10, 200, 2)
+    # bfo = OriginalBFO(problem_dict, epoch, pop_size)
+    # best_position_bfo, best_fit_bfo = bfo.solve()
+    # time_bfo = sum(bfo.history.list_epoch_time)
+    # print(f"BFO best position: {best_position_bfo} best fitness: {best_fit_bfo} time: {int(time_bfo)}")
+    # abc = BaseABC(problem_dict, epoch, pop_size)
+    # best_position_abc, best_fit_abc = abc.solve()
+    # time_abc = sum(abc.history.list_epoch_time)
+    # print(f"ABC best position: {best_position_abc} best fitness: {best_fit_abc} time: {int(time_abc)}")
 
-    bfo = OriginalBFO(problem_dict, epoch, pop_size)
-    best_position_bfo, best_fit_bfo = bfo.solve()
-    time_bfo = sum(bfo.history.list_epoch_time)
-    print(f"BFO best position: {best_position_bfo} best fitness: {best_fit_bfo} time: {int(time_bfo)}")
-    abc = OriginalBFO(problem_dict, epoch, pop_size)
-    best_position_abc, best_fit_abc = abc.solve()
-    time_abc = sum(abc.history.list_epoch_time)
-    print(f"ABC best position: {best_position_abc} best fitness: {best_fit_abc} time: {int(time_abc)}")
     # bfo.history.save_global_objectives_chart()
     # bfo.history.save_local_objectives_chart()
     # bfo.history.save_global_best_fitness_chart()
     # bfo.history.save_local_best_fitness_chart()
     # bfo.history.save_trajectory_chart()
-
